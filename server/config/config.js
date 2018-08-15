e117c3c54c4ac135685f74e48ad8927d10ba069e
Ayushi Negi <ayushin78@users.noreply.github.com>
2018-08-15 20:32:49 +0530

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -10,11 +10,3 @@ if(env === 'development' || env === 'test'){
   });
 
 }
-
-// if(env === 'development'){
-//   process.env.PORT = 3000;
-//   process.env.MONGOLAB_URI = 'mongodb://localhost:27017/TodoApp';
-// }else if(env === 'test'){
-//   process.env.PORT = 3000;
-//   process.env.MONGOLAB_URI = 'mongodb://localhost:27017/TodoAppTest';
-// }
